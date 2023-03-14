d50b0a2fb9087ec74fb0116f305ad1ae9e5aa529
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-03-14 17:10:33 -0300

    feat: req 2 done

--- src/routes/talkersRouter.js
+++ src/routes/talkersRouter.js
@@ -12,4 +12,16 @@ talkerManagerRouter.get('/', async (req, res) => {
   return res.status(200).json(talkerManager);
 });
 
+talkerManagerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerManager = await getAllTalker();
+  const talker = talkerManager.filter((talkers) => talkers.id === +id);
+  if (talker.length === 0) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  return res.status(200).json(talker[0]);
+});
+
 module.exports = talkerManagerRouter;
